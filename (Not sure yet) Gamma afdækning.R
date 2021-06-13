c5da1e8a498adcdc14d7e09a723208e9008d9c57
VNemirov <victor@nemiroff.dk>
2021-06-13 11:05:28 +0200

    Last fix

--- (Not sure yet) Gamma afdækning.R
+++ (Not sure yet) Gamma afdækning.R
@@ -1,11 +1,10 @@
-#Parameters
-endT = 1; sigma = 0.2; r = 0.05; K0 = 100 # Define K1 s.t. d1=0 (this maxes phi(d1) which will maximize Gamma)
+#Paramete
+endT = 1; sigma = 0.2; r = 0.05; K0 = 100; K1 = 110
 
 #Creating data
-# gamma_data <-
-  GBM_data(nsim = 1, setSeed = T, seed = "0806", dt = 1/1000, S0 = 100, sigma = 0.2) %>%
-    filter(time %in% c(0:4/4)) %>%
-    mutate(K1 = value*exp(sigma*(endT-time)+r*(endT-time)),
+gamma_data <- GBM_data(nsim = 1, setSeed = F, dt = 1/1000, S0 = 100, sigma = 0.1) %>%
+    # filter(time %in% c(0:4/4)) %>%
+    mutate(#K1 = value*exp(sigma*(endT-time)+r*(endT-time)),
            d1C = (log(value/K0)+(r+sigma^2/2)*(endT-time))/(sigma*sqrt(endT-time)),
            d2C = d1C-sigma*sqrt(endT-time),
            d1F = (log(value/K1)+(r+sigma^2/2)*(endT-time))/(sigma*sqrt(endT-time)),
@@ -14,38 +13,68 @@ endT = 1; sigma = 0.2; r = 0.05; K0 = 100 # Define K1 s.t. d1=0 (this maxes phi(
            DeltaF = pnorm(d1F),
            GammaC = dnorm(d1C)/(value*sigma*sqrt(endT-time)),
            GammaF = dnorm(d1F)/(value*sigma*sqrt(endT-time)),
-           BetaF = -GammaC/GammaF,
-           BetaS = -BetaF*DeltaF-DeltaC,
+           BetaF = round(-GammaC/GammaF,5),
+           BetaS = round(DeltaF*GammaC/GammaF-DeltaC,5),
            priceC = value*pnorm(d1C)-exp(-r*(endT-time))*K0*pnorm(d2C),
            priceF = value*pnorm(d1F)-exp(-r*(endT-time))*K1*pnorm(d2F),
            PnL = case_when(
-             time == 0 ~ 0,
-             time != 0 & time != 1 ~ priceC+BetaF*priceF+BetaS*value-(lag(priceC)+lag(BetaF)*lag(priceF)+lag(BetaS)*lag(value))*exp(r*(time-lag(time))),
-             time == 1 ~ lag(BetaF)*ifelse(value-lag(K1)<=0,0,value-lag(K1))+lag(BetaS)*value-(lag(priceC)+lag(BetaF)*lag(priceF)+lag(BetaS)*lag(value))*exp(r*(time-lag(time)))
-           ),
-           cumPnL = cumsum(PnL)) %>% tail()
-  
-  GBM_data(nsim = 1, setSeed = T, seed = "0806", dt = 1/1000, S0 = 100, sigma = 0.2) %>%
-    # filter(time %in% c(0:4/4)) %>%
-    mutate(K1 = value*exp(sigma*(endT-time)+r*(endT-time)),
-           d1C = (log(value/K0)+(r+sigma^2/2)*(endT-time))/(sigma*sqrt(endT-time)),
-           d2C = d1C-sigma*sqrt(endT-time),
-           d1F = (log(value/K1)+(r+sigma^2/2)*(1))/(sigma*sqrt(1)),
-           d2F = d1F-sigma*sqrt(1),
-           DeltaC = pnorm(d1C),
-           DeltaF = pnorm(d1F),
-           GammaC = dnorm(d1C)/(value*sigma*sqrt(endT-time)),
-           GammaF = dnorm(d1F)/(value*sigma*sqrt(1)),
-           BetaF = -GammaC/GammaF,
-           BetaS = -BetaF*DeltaF-DeltaC,
-           priceC = value*pnorm(d1C)-exp(-r*(endT-time))*K0*pnorm(d2C),
-           priceF = value*pnorm(d1F)-exp(-r*(1))*K1*pnorm(d2F),
-           cashflow = case_when(
+             BetaF == NaN | BetaS == NaN ~ 0,
              time == 0 ~ -(priceC+BetaF*priceF+BetaS*value),
-             time != 0 & time != 1 ~ (BetaF-lag(BetaF))*priceF+(BetaS-lag(BetaS))*value,
-             time == 1 ~ -lag(BetaF)*pmax(value-lag(K1),0)-lag(BetaS)*value
-           )) %>% tail()
-  
-  
-  
-  
+             time != 0 & time != 1 ~ -(BetaF-lag(BetaF))*priceF-(BetaS-lag(BetaS))*value,
+             time == 1 ~ lag(BetaF)*pmax(value-K1)+lag(BetaS)*value
+           ),
+           PnL = PnL*exp(r*(endT-time)),
+           cumPnL = cumsum(-PnL)) %>% tail()
+
+#all times
+set.seed("0806")
+Gamma_hedge <- data.frame(n = c(1:9,1:9*10,1:10*100), SE = c(1:9,1:9*10,1:10*100))
+for(j in c(1:9,1:9*10,1:10*100)){
+  print(paste("j:",j))
+  hedges <- c()
+  i<-0
+  while(length(hedges)<=1000){
+    i<-i+1
+    if(i %% 100 == 0){print(paste("i:", i))}
+    hedge <- GBM_data(nsim = 1, setSeed = F, dt = 1/j, S0 = 100, sigma = 0.2) %>%
+      # filter(time %in% c(0:4/4)) %>%
+      mutate(#K1 = value*exp(sigma*(endT-time)+r*(endT-time)),
+        d1C = (log(value/K0)+(r+sigma^2/2)*(endT-time))/(sigma*sqrt(endT-time)),
+        d2C = d1C-sigma*sqrt(endT-time),
+        d1F = (log(value/K1)+(r+sigma^2/2)*(endT-time))/(sigma*sqrt(endT-time)),
+        d2F = d1F-sigma*sqrt(endT-time),
+        DeltaC = pnorm(d1C),
+        DeltaF = pnorm(d1F),
+        GammaC = dnorm(d1C)/(value*sigma*sqrt(endT-time)),
+        GammaF = dnorm(d1F)/(value*sigma*sqrt(endT-time)),
+        BetaF = round(-GammaC/GammaF,5),
+        BetaS = round(DeltaF*GammaC/GammaF-DeltaC,5),
+        priceC = value*pnorm(d1C)-exp(-r*(endT-time))*K0*pnorm(d2C),
+        priceF = value*pnorm(d1F)-exp(-r*(endT-time))*K1*pnorm(d2F),
+        PnL = case_when(
+          BetaF == NaN | BetaS == NaN ~ 0,
+          time == 0 ~ -(priceC+BetaF*priceF+BetaS*value),
+          time != 0 & time != 1 ~ -(BetaF-lag(BetaF))*priceF-(BetaS-lag(BetaS))*value,
+          time == 1 ~ lag(BetaF)*pmax(value-K1)+lag(BetaS)*value
+        ),
+        PnL = PnL*exp(r*(endT-time)),
+        cumPnL = cumsum(-PnL))
+    beta <- hedge %>% pull(BetaF) %>% abs() %>% max(na.rm = T)
+    error <- abs(max(hedge %>% pull(value) %>% last()-K0,0)-hedge %>% pull(cumPnL) %>% last())
+    
+    if(beta >= 1.5 | is.na(beta)){
+      # print(paste("error:",error))
+      next
+    }
+    else{
+      hedges <- c(hedges, error)
+    }
+  }
+  Gamma_hedge[Gamma_hedge$n == j,]$SE <- sd(hedges, na.rm = TRUE)
+}
+
+
+ggplot(Gamma_hedge, aes(x = n, y = SE))+
+  geom_point()+
+  scale_x_log10()+
+  scale_y_log10()
--- Scripts/3. Lukkede MLE.R
+++ Scripts/3. Lukkede MLE.R
@@ -1,5 +1,5 @@
 #Lukkede MLE
-#All Data:
+#All Data (S&P500):
 m_hat <- mean(df_sp500$log_increments)
 ssquared_hat <- 1/(length(df_sp500$log_increments)-1)*sum((df_sp500$log_increments-m_hat)^2)
 
@@ -21,9 +21,36 @@ mu_hat <- (m+v^2/2)*nrow(df_sp500)
 s_hat <- v/sqrt(1/nrow(df_sp500))
 
 
-MLE_data <- data.frame(
+MLE_dataSP500 <- data.frame(
   est = c("mu", "sigma"),
   `2020` = c(mu_hat_2020, s_hat_2020),
   `Al data` = c(mu_hat/(11+1/12), s_hat/sqrt(11+1/12))
 )
 
+#Russel
+m_hat <- mean(df_rus2000$log_increments)
+ssquared_hat <- 1/(length(df_rus2000$log_increments)-1)*sum((df_rus2000$log_increments-m_hat)^2)
+
+mu_hat <- (m_hat+ssquared_hat/2)/252
+sigma_hat <- sqrt(ssquared_hat)/252
+
+
+#2020 Data:
+df_rus2000_2020 <- filter(df_rus2000, substr(date, 0, 4) == "2020")
+m_2020 <- mean(df_rus2000_2020$log_increments)
+v_2020 <- sqrt(1/(nrow(df_rus2000_2020)-1)*sum((df_rus2000_2020$log_increments-m_2020)^2))
+mu_hat_2020 <- (m_2020+v_2020^2/2)*nrow(df_rus2000_2020)
+s_hat_2020 <- v_2020/sqrt(1/nrow(df_rus2000_2020))
+
+#Test change
+m <- mean(df_rus2000$log_increments)
+v <- sqrt(1/(nrow(df_rus2000)-1)*sum((df_rus2000$log_increments-m)^2))
+mu_hat <- (m+v^2/2)*nrow(df_rus2000)
+s_hat <- v/sqrt(1/nrow(df_rus2000))
+
+
+MLE_dataRUS2000 <- data.frame(
+  est = c("mu", "sigma"),
+  `2020` = c(mu_hat_2020, s_hat_2020),
+  `Al data` = c(mu_hat/(11+1/12), s_hat/sqrt(11+1/12))
+)
